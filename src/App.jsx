6158e86ecf258783eae464d23e47d6a67c354792
aghamyan <57287656+aghamyan@users.noreply.github.com>
2026-02-03 16:00:10 +0400

    Add signup section layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import FeaturesSection from './components/sections/FeaturesSection.jsx';
 import HowItWorksSection from './components/sections/HowItWorksSection.jsx';
 import ResultsSection from './components/sections/ResultsSection.jsx';
 import AboutSection from './components/sections/AboutSection.jsx';
+import SignupSection from './components/sections/SignupSection.jsx';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <HowItWorksSection />
         <ResultsSection />
         <AboutSection />
+        <SignupSection />
       </main>
     </div>
   );
